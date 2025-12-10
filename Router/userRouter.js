f89247d6d1076cf36f3627b76b210bd665b48e26
Rahinmon2903 <rahin2903@gmail.com>
2025-12-10 11:42:40 +0530

    login and mailerjs completed

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -1,9 +1,11 @@
 import express from "express"
-import { Register } from "../Controller/authController.js";
+import { login, Register } from "../Controller/authController.js";
 
 
 const userRouter=express.Router();
 
 userRouter.post("/register",Register)
+
+userRouter.post("/login",login)
 
 export default userRouter;
