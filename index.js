e999926b3114f79382790d8621c45276115cbbb9
Rahinmon2903 <rahin2903@gmail.com>
2025-12-11 18:48:41 +0530

    cors updated

--- index.js
+++ index.js
@@ -4,36 +4,42 @@ import cors from "cors";
 import dbConnect from "./database/dbConfig.js";
 import userRouter from "./Router/userRouter.js";
 
-
 dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 5000;
 
-const port = process.env.PORT
-
+// Middleware
 app.use(express.json());
-app.use(cors({
+
+
+app.use(
+  cors({
     origin: [
-        "https://restpass-front-end.vercel.app",
-        "https://restpass-front-jrfmc1laf-rahin-mon-ss-projects.vercel.app"
+      "https://restpass-front-end.vercel.app",
+      "https://restpass-front-jrfmc1laf-rahin-mon-ss-projects.vercel.app",
+      "https://restpass-front-q131twzjk-rahin-mon-ss-projects.vercel.app",
+      "http://localhost:5173"  
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    credentials: true
-}));
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
+// Database connection
 dbConnect();
 
+
 app.get("/", (req, res) => {
-    res.status(200).send("Welcome to my API")
+  res.status(200).send("Welcome to my API");
+});
 
-
-})
-
+// API routes
 app.use("/api/auth", userRouter);
 
+
+
 app.listen(port, () => {
-    console.log("server started");
-
-})
-
-
+  console.log("server started on port", port);
+});
