c699e810385a0d208671aa7593de6c999a0ea491
Rahinmon2903 <rahin2903@gmail.com>
2025-12-31 18:52:39 +0530

    used different package instead of nodemailer

--- Utils/mailer.js
+++ Utils/mailer.js
@@ -1,25 +1,41 @@
-import nodemailer from "nodemailer";
+
+import SibApiV3Sdk from "sib-api-v3-sdk";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    auth: {
-        user: process.env.PASS_MAIL,
-        pass: process.env.PASS_KEY,
-    },
-});
 
-const sendEmail=async (to,subject,text) => {
-    const mailOptions={
-        from:process.env.PASS_MAIL,
-        to,
-        subject,
-        text
-    }
-    return await transporter.sendMail(mailOptions);
-    
-}
+const client = SibApiV3Sdk.ApiClient.instance;
+const apiKey = client.authentications["api-key"];
+apiKey.apiKey = process.env.BREVO_API_KEY; 
+
+const tranEmailApi = new SibApiV3Sdk.TransactionalEmailsApi();
+
+
+const sendEmail = async (to, subject, text) => {
+  try {
+    const emailData = {
+      sender: {
+        name: "Rahin Mon",
+        email: process.env.PASS_MAIL,
+      },
+      to: [
+        {
+          email: to,
+        },
+      ],
+      subject: subject,
+      textContent: text,
+    };
+
+    await tranEmailApi.sendTransacEmail(emailData);
+    console.log("Email sent successfully via Brevo");
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw error;
+  }
+};
 
 export default sendEmail;
+
+
